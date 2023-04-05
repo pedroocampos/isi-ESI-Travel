f994bf1a43e04f4e4056e08972b637dffcec43d3
Pedro Campos Castellanos <pedro.campos@alu.uclm.es>
2023-04-05 12:51:33 +0200

    Empezando con Flask

---
+++ src/app.py
@@ -0,0 +1,45 @@
+# Archivo para ir haciendo pruebas con Flask y SQLite
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(80))
+    email = db.Column(db.String(100))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def iniciar_sesion():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def comprobar_usuario():
+    correo_electronico = request.form["email_login"]
+    contraseña = request.form["contraseña_login"]
+
+    usuario = User.query.filter(User.email == correo_electronico, User.password == contraseña).first()
+    if not usuario:
+        return render_template("login.html", error="Correo electrónico o contraseña incorrecto")
+    else:
+        return render_template("index.html")
+
+@app.route("/registro")
+def registrar():
+    return render_template("registro.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
